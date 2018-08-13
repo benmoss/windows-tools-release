577270042b6e91b38de9c3681819b261a02c6569
Sam Smith <sesmith177@gmail.com>
2018-08-13 17:01:34 -0400

    wip
    - bazel, msys2, vs_buildtools
    
    Signed-off-by: Arjun Sreedharan <asreedharan@pivotal.io>

---
+++ jobs/msys2/templates/pre-start.ps1
@@ -0,0 +1,33 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$mtx = New-Object System.Threading.Mutex($false, "PathMutex")
+
+if (!$mtx.WaitOne(5000)) {
+  throw "Could not acquire PATH mutex"
+}
+
+$OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+$AddedFolder="c:\var\vcap\packages\msys2"
+
+if (-not $OldPath.Contains($AddedFolder)) {
+  $NewPath=$AddedFolder+';'+$OldPath
+  Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+}
+
+#https://github.com/msys2/msys2/wiki/MSYS2-installation
+Write-Host "Starting initialization via msys2_shell.cmd"
+Start-Process "$AddedFolder\msys2_shell.cmd" -Wait -ArgumentList '-c', exit
+
+Write-Host "Repeating system update until there are no more updates or max 5 iterations"
+$ErrorActionPreference = 'Continue'
+Set-Alias bash "$AddedFolder\usr\bin\bash.exe"
+while (!$done) {
+    Write-Host "`n================= SYSTEM UPDATE $((++$i)) =================`n"
+    bash -lc 'pacman --noconfirm -Syuu | tee /update.log'
+    $done = (Get-Content $AddedFolder\update.log) -match 'there is nothing to do' | Measure-Object | ForEach-Object { $_.Count -eq 2 }
+    $done = $done -or ($i -ge 5)
+}
+Remove-Item $AddedFolder\update.log -ea 0
+
+$mtx.ReleaseMutex()
---
+++ jobs/vs_buildtools/templates/pre-start.ps1
@@ -0,0 +1,19 @@
+$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$mtx = New-Object System.Threading.Mutex($false, "PathMutex")
+
+if (!$mtx.WaitOne(5000)) {
+  throw "Could not acquire PATH mutex"
+}
+
+$OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+$vcRedistDir = "C:\var\vcap\packages\vs_buildtools\VC\Redist\MSVC\14.14.26405\x64\Microsoft.VC141.CRT"
+
+if (-not $OldPath.Contains($vcRedistDir)) {
+  $NewPath=$OldPath+';'+$vcRedistDir
+  Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+}
+
+$mtx.ReleaseMutex()
+
