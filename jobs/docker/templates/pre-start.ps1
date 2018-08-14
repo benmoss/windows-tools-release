3d0119a95c1c01023959ddf7b36faf859e58b1d5
Sam Smith <sesmith177@gmail.com>
2018-08-14 16:24:37 -0400

    wip
    Signed-off-by: Arjun Sreedharan <asreedharan@pivotal.io>

--- jobs/docker/templates/pre-start.ps1
+++ jobs/docker/templates/pre-start.ps1
@@ -3,7 +3,7 @@ trap { $host.SetShouldExit(1) }
 
 $mtx = New-Object System.Threading.Mutex($false, "PathMutex")
 
-if (!$mtx.WaitOne(5000)) {
+if (!$mtx.WaitOne(300000)) {
   throw "Could not acquire PATH mutex"
 }
 
@@ -18,6 +18,6 @@ if (-not $OldPath.Contains($AddedFolder)) {
 
 $mtx.ReleaseMutex()
 
-dockerd --register-service
+&"$AddedFolder\dockerd" --register-service
 Start-Service Docker
 
--- jobs/msys2/templates/pre-start.ps1
+++ jobs/msys2/templates/pre-start.ps1
@@ -3,31 +3,31 @@ trap { $host.SetShouldExit(1) }
 
 $mtx = New-Object System.Threading.Mutex($false, "PathMutex")
 
-if (!$mtx.WaitOne(5000)) {
+if (!$mtx.WaitOne(300000)) {
   throw "Could not acquire PATH mutex"
 }
 
 $OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
-$AddedFolder="c:\var\vcap\packages\msys2"
+$msys2UsrBin="c:\var\vcap\packages\msys2\usr\bin"
 
-if (-not $OldPath.Contains($AddedFolder)) {
-  $NewPath=$AddedFolder+';'+$OldPath
+if (-not $OldPath.Contains($msys2UsrBin)) {
+  $NewPath=$msys2UsrBin+';'+$OldPath
   Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
 }
 
 #https://github.com/msys2/msys2/wiki/MSYS2-installation
 Write-Host "Starting initialization via msys2_shell.cmd"
-Start-Process "$AddedFolder\msys2_shell.cmd" -Wait -ArgumentList '-c', exit
+Start-Process "c:\var\vcap\packages\msys2\msys2_shell.cmd" -Wait -ArgumentList '-c', exit
 
 Write-Host "Repeating system update until there are no more updates or max 5 iterations"
 $ErrorActionPreference = 'Continue'
-Set-Alias bash "$AddedFolder\usr\bin\bash.exe"
+Set-Alias bash "$msys2UsrBin\bash.exe"
 while (!$done) {
     Write-Host "`n================= SYSTEM UPDATE $((++$i)) =================`n"
     bash -lc 'pacman --noconfirm -Syuu | tee /update.log'
-    $done = (Get-Content $AddedFolder\update.log) -match 'there is nothing to do' | Measure-Object | ForEach-Object { $_.Count -eq 2 }
+    $done = (Get-Content "c:\var\vcap\packages\msys2\update.log") -match 'there is nothing to do' | Measure-Object | ForEach-Object { $_.Count -eq 2 }
     $done = $done -or ($i -ge 5)
 }
-Remove-Item $AddedFolder\update.log -ea 0
+Remove-Item "c:\var\vcap\packages\msys2\update.log" -ea 0
 
 $mtx.ReleaseMutex()
