40ba7734b413896422b9fd0a56fb381d10ba8e3c
Arjun Sreedharan <asreedharan@pivotal.io>
2018-08-14 17:55:45 -0400

    wip
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- jobs/python/templates/pre-start.ps1
+++ jobs/python/templates/pre-start.ps1
@@ -7,11 +7,14 @@ if (!$mtx.WaitOne(300000)) {
   throw "Could not acquire PATH mutex"
 }
 
-$OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
-$AddedFolder="c:\var\vcap\packages\python"
+$installDir = "c:\python27"
+$msiFile = (Get-ChildItem "c:\var\vcap\packages\python/*.msi").FullName
+Start-Process -FilePath msiexec -ArgumentList "/i $msiFile /qn ALLUSERS=1 ADDLOCAL=ALL TargetDir=$installDir" -Wait -NoNewWindow
 
-if (-not $OldPath.Contains($AddedFolder)) {
-  $NewPath=$OldPath+';'+$AddedFolder
+$OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+
+if (-not $OldPath.Contains($installDir)) {
+  $NewPath=$OldPath+';'+$installDir
   Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
 }
 
